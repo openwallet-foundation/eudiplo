a1e2408305e58afce70f900cfdbdf3d4824f594b
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-07-02 14:09:17 +0200

    fix: only run oidf tests
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- apps/backend/test/vitest.config.ts
+++ apps/backend/test/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        include: ["**/*.e2e-spec.ts"],
+        include: ["**/oidf/*.e2e-spec.ts"],
         exclude: [],
         globals: true,
         root: "./",
