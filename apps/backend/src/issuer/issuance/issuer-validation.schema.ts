2ae42c8fc944b80183713835691ed298f9282540
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-07-22 13:12:02 +0200

    fix: align issuer schema handling
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- apps/backend/src/issuer/issuance/issuer-validation.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from "joi";
-
-export const ISSUER_VALIDATION_SCHEMA = Joi.object({
-    ISSUER_MULTI_CONSUMPTION: Joi.boolean()
-        .default(false)
-        .description("Enable or disable multi-consumption for the issuer")
-        .meta({ group: "issuer", order: 10 }),
-});
